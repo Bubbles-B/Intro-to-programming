8bad5bf4ff609abfadbdcaba0666f9d86301072a
Bubbles <sediiskaki@gmail.com>
2024-11-10 15:11:47 +0200

    Add files via upload

---
+++ JavaScript For Kids/Chapter 8/Programming Challenge #1.js
@@ -0,0 +1,16 @@
+var add = function (a, b) {
+  return a + b;
+};
+
+var multiply = function (a, b) {
+  return a * b;
+};
+
+add(multiply(36325, 9824), 777);
+
+
+
+//var add = (a, b) => a + b;
+//var multiply = (a, b) => a * b;
+//console.log(add(multiply(36325, 9824), 777));
+
